8846d271ef57fe61358aaa066adc9e07c57ecd69
LuisFelipeUrena <60860402+LuisFelipeUrena@users.noreply.github.com>
2020-09-16 19:28:03 -0400

    stack pointer and mul ready to go

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,16 +7,17 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 8
+        self.ram = [0] * 256
         self.pc = 0
         self.reg = [0] * 8
+        self.sp = 7
     
     
     def ram_read(self,address):
         '''
         Read the information at the index in the RAM
         '''
-        return print(self.ram[address])
+        return self.ram[address]
     
     
     def ram_write(self,value,address):
@@ -27,24 +28,29 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
+        if len(sys.argv) != 2:
+            print('usage: ls8.py filename')
+            sys.exit(1)
+        try:
+            address = 0
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    t = line.split('#')
+                    n = t[0].strip()
+                    if n == '':
+                        continue
+                    try:
+                        n = '0b' + n
+                        n = int(n,2)
+                    except TypeError:
+                        print(f'Number not valid {n}')
+                        sys.exit(1)
 
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    self.ram_write(n,address)
+                    address += 1
+        except FileNotFoundError:
+            print('file not found')
+            sys.exit(2)                
 
 
     def alu(self, op, reg_a, reg_b):
@@ -79,25 +85,71 @@ class CPU:
     def run(self):
         """Run the CPU."""
         running = True
-        self.pc = 0
+        # instructions!
+        LDI = 0b10000010 
+        PRN = 0b01000111
+        MUL = 0b10100010
+        HLT = 0b00000001
+        PUSH = 0b01000101
+        POP = 0b01000110
+
         
         while running:
             ir = self.ram[self.pc]
             
             
-            if ir == 130:
-                self.ram_write(8,0)
+            if ir == LDI:
+                reg_num = self.ram[self.pc + 1]
+                value = self.ram[self.pc + 2]
+                self.reg[reg_num] = value
                 self.pc +=3
             
-            if ir ==  71:
-                self.ram_read(0)
+            elif ir ==  PRN:
+                reg_num = self.ram[self.pc + 1]
+                print(self.reg[reg_num])
                 self.pc += 2
             
-            if ir ==  1:
+            elif ir ==  HLT:
                 running = False
+
+            elif ir == MUL:
+                r1 = self.ram[self.pc + 1]
+                r2 = self.ram[self.pc + 2]
+                print(self.reg[r1] * self.reg[r2])
+                self.pc += 3
+
+            elif ir == POP:
+                reg_num = self.ram[self.pc + 1]
+                top_stack = self.reg[self.sp]
+
+                value = self.ram[top_stack]
+                self.reg[reg_num] = value
+                self.reg[self.sp] += 1
+                
+                self.pc += 2
+            
+            elif  ir == PUSH:
+                self.reg[self.sp] -= 1
+                reg_num = self.ram[self.pc + 1]
+                value = self.reg[reg_num]
+
+
+                top_stack = self.reg[self.sp]
+                self.ram[top_stack] = value
+                self.pc += 2
+
+
+
+
+
+
             
             else:
-                print('unknown instruction')    
+                print('unknown instruction')
+                sys.exit(3)  
+
+
+    
 
 
 
